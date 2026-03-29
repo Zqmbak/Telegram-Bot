867a2f9ecdde467be3a2febf855bd946a44c1a80
Zqmbak <167011451+Zqmbak@users.noreply.github.com>
2026-03-29 19:51:15 +0300

    Add files via upload

---
+++ main для гита.py
@@ -0,0 +1,51 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+menu_keyboard = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [types.KeyboardButton(text="Записаться!")]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите действие..."  # подсказка в поле ввода
+)
+
+menu_uslugi = types.ReplyKeyboardMarkup(
+    keyboard=[
+        [types.KeyboardButton(text="Борода")],
+        [types.KeyboardButton(text="Стрижка")],
+        [types.KeyboardButton(text="Укладка")]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите действие..."  # подсказка в поле ввода
+)
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(
+        "👋 Добро пожаловать в барбершоп (Барби)!\n\n"
+        "Выбери действие в меню ниже:",
+        reply_markup=menu_keyboard
+    )
+
+@dp.message(lambda m: m.text == "Записаться!")
+async def book(message: types.Message):
+    reply_markup=menu_uslugi
+
+@dp.message(lambda m: m.text == "Борода")
+async def Boroda(message: types.Message):
+    await message.answer("ОООО, Так ты Дворф!!!")
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
